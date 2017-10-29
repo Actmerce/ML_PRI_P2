a96582455c68f3c0ac25ec588fe5813bc69b8e70
你的老板 <417638196@qq.com>
2017-10-29 11:32:37 +0800

    第二次提交/增加了电影简介跟一些代码注释

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,18 +1,27 @@
 # -*- coding:utf-8 -*-
 # __author__ = 'ActMerce'
 # __date__ = '2017/10/28 21:18'
-import fresh_tomatoes, media
+import fresh_tomatoes
+import media
 
 
 # 电影列表
-what_your_name = media.Movie('君の名は', 'https://i.ytimg.com/vi/sjhbO8lIc2s/movieposter.jpg',
-                             'https://www.youtube.com/watch?v=sjhbO8lIc2s&list=PLHPTxTxtC0iaN9kA37m6MRrxFkgby2CDR')
-harry_potter = media.Movie('Harry Potter', 'https://i.ytimg.com/vi/8GKHFtMaoOY/movieposter.jpg',
-                           'https://www.youtube.com/watch?v=8GKHFtMaoOY&list=PLHPTxTxtC0iaN9kA37m6MRrxFkgby2CDR')
-jin_sang = media.Movie('银魂', 'https://i.ytimg.com/vi/wyFQ2aQL6b4/movieposter.jpg',
-                       'https://www.youtube.com/watch?v=wyFQ2aQL6b4&list=PLxaUumW-X5DcGVwlxF7IJEPKwUw74NiPq')
-dead_pool = media.Movie('死侍', 'https://i.ytimg.com/vi/MD-VMxmWezE/movieposter.jpg',
-                        'https://www.youtube.com/watch?v=MD-VMxmWezE&list=PLAXJzGOFS3xOOJXl2dJAeUg92LZ-WNrSX')
+what_your_name = media.Movie('君の名は',
+                             '作品讲述了男女高中生在梦中相遇，并寻找彼此的故事。',
+                             'https://i.ytimg.com/vi/sjhbO8lIc2s/movieposter.jpg',
+                             'https://www.youtube.com/watch?v=sjhbO8lIc2s&list=PLHPTxTxtC0iaN9kA37m6MRrxFkgby2CDR')  # noqa
+harry_potter = media.Movie('Harry Potter',
+                           '描写的是年轻的巫师学生哈利·波特在霍格沃茨前后六年的学习生活和冒险故事',
+                           'https://i.ytimg.com/vi/8GKHFtMaoOY/movieposter.jpg',
+                           'https://www.youtube.com/watch?v=8GKHFtMaoOY&list=PLHPTxTxtC0iaN9kA37m6MRrxFkgby2CDR')  # noqa
+jin_sang = media.Movie('银魂',
+                       '江户时代末期，有一个武士与同伴愉快地过着异想天开的生活。',
+                       'https://i.ytimg.com/vi/wyFQ2aQL6b4/movieposter.jpg',
+                       'https://www.youtube.com/watch?v=wyFQ2aQL6b4&list=PLxaUumW-X5DcGVwlxF7IJEPKwUw74NiPq')  # noqa
+dead_pool = media.Movie('死侍',
+                        '死侍（Deadpool）是美国漫威漫画旗下反英雄',
+                        'https://i.ytimg.com/vi/MD-VMxmWezE/movieposter.jpg',
+                        'https://www.youtube.com/watch?v=MD-VMxmWezE&list=PLAXJzGOFS3xOOJXl2dJAeUg92LZ-WNrSX')  # noqa
 
 # 集
 movies = [what_your_name, harry_potter, jin_sang, dead_pool]
--- media.py
+++ media.py
@@ -4,7 +4,16 @@
 
 
 class Movie(object):
-    def __init__(self, title, poster_image_url, trailer_youtube_url):
+    """喜爱的电影"""
+    def __init__(self, title, intro, poster_image_url, trailer_youtube_url):
+        """
+        构造函数
+        :param title:电影的标题
+        :param intro:电影的简介
+        :param poster_image_url:缩略图url
+        :param trailer_youtube_url: 预告片url(须要油管源)
+        """
         self.title = title
+        self.intro = intro
         self.poster_image_url = poster_image_url
         self.trailer_youtube_url = trailer_youtube_url
